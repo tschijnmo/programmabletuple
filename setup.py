64efdb6058cc704bf23acbe3ac0ff0be22ae4a69
Tschijnmo TSCHAU <tschijnmotschau@gmail.com>
2014-12-14 19:52:35 -0600

    Add a basic setup.py script
    Just some basic information is put into it. The basic distutils is used
    without trying to import setuputils, due to the low level of the
    package.

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='immutableclass',
+      version='0.1',
+      description='Python metaclass for making instances of user-defined classes immutable',
+      author='Tschijnmo TSCHAU',
+      author_email='tschijnmotschau@gmail.com',
+      url='https://github.com/tschijnmo/immutableclass',
+      py_modules=['immutableclass', ],
+     )
+
